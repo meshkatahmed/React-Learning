e4d0223fd6f89cb1fd54920a34b06ca74a78233d
Meshkat <meshkatuddinahammed@outlook.com>
2023-08-30 16:05:39 +0600

    Lesson-7

--- vite-project/src/components/Clock.jsx
+++ vite-project/src/components/Clock.jsx
@@ -1,24 +1,28 @@
 import { Component } from 'react';
 
 class Clock extends Component {
-    constructor(props) {
-        super(props);
-        this.state = {date: new Date()};
-    }
-    componentDidMount() {
-        setInterval(() => {
-            this.setState({date: new Date()});
-        });
-    }
-    render() {
-      return (
-        <h1 className='heading'>
-          <span className='text'>
-            {this.state.date.toLocaleTimeString(this.props.locale)}
-          </span>
-        </h1>
-      ); 
-    }
+  state = {date: new Date()};
+  // constructor(props) {
+  //   super(props);
+  // }
+  componentDidMount() {
+    this.clockTimer = setInterval(() => this.tick(),1000);
+  };
+  tick() {
+    this.setState({date: new Date()});
+  };
+  componentWillUnmount() {
+    clearInterval(this.clockTimer);
+  };
+  render() {
+    return (
+      <h1 className='heading'>
+        <span className='text'>
+          {this.state.date.toLocaleTimeString(this.props.locale)}
+        </span>
+      </h1>
+    ); 
+  }
 }
 
 export default Clock;
